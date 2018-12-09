a37d09062155fc9ab63c071e677b76463b4580dd
Will Crichton <wcrichto@cs.stanford.edu>
2018-12-09 22:55:19 +0000

    Restore parallel build

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -3,10 +3,10 @@
 # Prepare Kaldi
 cd kaldi/tools
 make clean
-make
+make -j ${cores}
 ./extras/install_openblas.sh
 cd ../src
 make clean
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
-make depend
+make -j ${cores} depend
 cd ../../
