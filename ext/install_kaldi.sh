cec4364e44cf4fe1496f9feb579f436b5e082968
Will Crichton <wcrichto@cs.stanford.edu>
2018-12-09 23:10:59 +0000

    No make clean

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -2,11 +2,11 @@
 
 # Prepare Kaldi
 cd kaldi/tools
-make clean
+#make clean
 make -j ${cores}
 ./extras/install_openblas.sh
 cd ../src
-make clean
+#make clean
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
 make -j ${cores} depend
 cd ../../
