599f0f0cbaefff8a6c3d3ce923ee843a5528e92a
Jeremy Ephron <jeremyephron@gmail.com>
2020-02-28 12:29:29 +0000

    added missing required dependencies to install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -7,9 +7,14 @@ echo "Installing dependencies..."
 # Install OS-specific dependencies
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	apt-get update -qq
-	apt-get install -y zlib1g-dev automake autoconf git \
-		libtool subversion libatlas3-base python3-pip \
-		python3-dev wget unzip python3
+	apt-get install -y \
+        automake autoconf \
+        zlib1g-dev libtool
+		git subversion \
+        libatlas3-base \
+        python python-dev python-pip \
+		python3 python3-dev python3-pip \
+        wget unzip
 	apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
 	python3 setup.py develop
 elif [[ "$OSTYPE" == "darwin"* ]]; then
