9582c188a6b0dd20a8d1b1cacb42dda052252111
Jeremy Ephron Barenholtz <jeremyephron@gmail.com>
2020-02-28 05:00:17 -0800

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -9,11 +9,11 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	apt-get update -qq
 	apt-get install -y \
         automake autoconf \
-        zlib1g-dev libtool
-		git subversion \
+        zlib1g-dev libtool \
+	git subversion \
         libatlas3-base \
         python python-dev python-pip \
-		python3 python3-dev python3-pip \
+	python3 python3-dev python3-pip \
         wget unzip
 	apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
 	python3 setup.py develop
